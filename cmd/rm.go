1d023aac6a6123e21d355f1ee8b2ac5dfe7e13a4
Spyros Mouchlianitis <spirosgsaaa@gmail.com>
2024-08-17 19:44:07 +0300

    refactor: del -> rm

--- cmd/rm.go
+++ cmd/rm.go
@@ -9,32 +9,32 @@ import (
 	"log"
 )
 
-// delCmd represents the del command
-var delCmd = &cobra.Command{
-	Use:   "del",
-	Short: "Deletes a given secret",
+// rmCmd represents the rm command
+var rmCmd = &cobra.Command{
+	Use:   "rm",
+	Short: "Removes a secret",
 	Long:  ``,
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		err := DeleteSecret(args[0])
 		if err != nil {
-			log.Fatalf(err.Error())
+			log.Fatal(err)
 		}
 
-		fmt.Println("Secret " + args[0] + " deleted")
+		fmt.Println("Deleted secret:", args[0])
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(delCmd)
+	rootCmd.AddCommand(rmCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// delCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// rmCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// delCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rmCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
